02d18a52dba908d4b1c141dbe0042bbd8ddf8d53
khreeva <khreeva27@yandex.ru>
2024-05-14 21:18:37 +0300

    Добавлена функциональность для отображения данных из файла Excel в Table_DataGridView Открытие файла Excel через OpenFileDialog и чтение данных из выбранного файла. Данные из первого листа Excel файла считываются и отображаются в Table_DataGridView.
    Примечание: добавь срочно библиотеку ExcelDataReader в проект.

--- VSC/Form1.cs
+++ VSC/Form1.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using ExcelDataReader;
 
 namespace VSC
 {
@@ -27,6 +22,26 @@ namespace VSC
             {
                 string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(openFileDialog.FileName); // Получаем имя файла без расширения
                 Info_TextBox.Text = fileNameWithoutExtension;
+
+                // Чтение данных из файла Excel и отображение в DataGridView
+                using (var stream = File.Open(openFileDialog.FileName, FileMode.Open, FileAccess.Read))
+                {
+                    using (var reader = ExcelReaderFactory.CreateReader(stream))
+                    {
+                        // Используем AsDataSet для получения всех данных из Excel в DataSet
+                        var result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                        {
+                            ConfigureDataTable = (_) => new ExcelDataTableConfiguration() { UseHeaderRow = true }
+                        });
+
+                        // Берем первый лист и привязываем его к DataGridView
+                        if (result.Tables.Count > 0)
+                        {
+                            DataTable dataTable = result.Tables[0];
+                            Table_DataGridView.DataSource = dataTable;
+                        }
+                    }
+                }
             }
         }
     }
